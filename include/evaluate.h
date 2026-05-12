9a194627575ca516ae284c408c3e8d148d1aeb34
Claudio M. Camacho <claudiomkd@gmail.com>
2026-05-12 20:04:52 +0300

    Add missing NNUE evaluation files.

--- include/evaluate.h
+++ include/evaluate.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "position.h"
+#include "nnue_bridge.h"
 
 extern const int PieceValues[7];
+extern bool nnue_initialized;
 
 Value evaluate(const Position& pos);
--- include/nnue.h
+++ include/nnue.h
@@ -1,57 +1,155 @@
-#pragma once
+#ifndef NNUE_H
+#define NNUE_H
 
-#include <cstdint>
-#include <string>
-#include <algorithm>
-#include "types.h"
+#ifndef __cplusplus
+#ifndef _MSC_VER
+#include <stdalign.h>
+#endif
+#endif
 
+/**
+* Calling convention
+*/
+#ifdef __cplusplus
+#   define EXTERNC extern "C"
+#else
+#   define EXTERNC
+#endif
 
-// ---------------------------------------------------------------------------
-//  HalfKP Dimensions
-// ---------------------------------------------------------------------------
-constexpr int NNUE_PIECES = 7;
-constexpr int NNUE_SQUARES = 64;
-constexpr int TRANSFORMER_INPUTS = NNUE_PIECES * NNUE_SQUARES * 2;
-constexpr int TRANSFORMER_NEURONS = 256;
+#if defined (_WIN32)
+#   define _CDECL __cdecl
+#ifdef DLL_EXPORT
+#   define DLLExport EXTERNC __declspec(dllexport)
+#else
+#   define DLLExport EXTERNC __declspec(dllimport)
+#endif
+#else
+#   define _CDECL
+#   define DLLExport EXTERNC
+#endif
 
-// ---------------------------------------------------------------------------
-//  Accumulator (64-byte aligned for cache-line safety)
-// ---------------------------------------------------------------------------
-struct alignas(64) Accumulator {
-    int16_t white[TRANSFORMER_NEURONS];
-    int16_t black[TRANSFORMER_NEURONS];
+/**
+* Internal piece representation
+*     wking=1, wqueen=2, wrook=3, wbishop= 4, wknight= 5, wpawn= 6,
+*     bking=7, bqueen=8, brook=9, bbishop=10, bknight=11, bpawn=12
+*
+* Make sure the piecesyou pass to the library from your engine
+* use this format.
+*/
+enum colors {
+    white,black
+};
+enum pieces {
+    blank=0,wking,wqueen,wrook,wbishop,wknight,wpawn,
+            bking,bqueen,brook,bbishop,bknight,bpawn
 };
 
-// ---------------------------------------------------------------------------
-//  Global Network Weights
-// ---------------------------------------------------------------------------
-extern int16_t ft_weights[TRANSFORMER_INPUTS][TRANSFORMER_NEURONS];
-extern int16_t ft_biases[TRANSFORMER_NEURONS];
+/**
+* nnue data structure
+*/
 
-extern int8_t output_weights[TRANSFORMER_NEURONS * 2];
-extern int32_t output_bias;
+typedef struct DirtyPiece {
+  int dirtyNum;
+  int pc[3];
+  int from[3];
+  int to[3];
+} DirtyPiece;
 
-extern bool nnue_initialized;
+typedef struct Accumulator {
+  alignas(64) int16_t accumulation[2][256];
+  int computedAccumulation;
+} Accumulator;
 
-// ---------------------------------------------------------------------------
-//  Loader
-// ---------------------------------------------------------------------------
-bool load_nnue(const std::string& filepath);
+typedef struct NNUEdata {
+  Accumulator accumulator;
+  DirtyPiece dirtyPiece;
+} NNUEdata;
 
-// ---------------------------------------------------------------------------
-//  Activation
-// ---------------------------------------------------------------------------
-constexpr int32_t clipped_relu(int16_t x) {
-    return std::max(int32_t(0), std::min(int32_t(127), static_cast<int32_t>(x)));
-}
+/**
+* position data structure passed to core subroutines
+*  See @nnue_evaluate for a description of parameters
+*/
+typedef struct Position {
+  int player;
+  int* pieces;
+  int* squares;
+  NNUEdata* nnue[3];
+} Position;
 
-// ---------------------------------------------------------------------------
-//  Accumulator Refresh
-// ---------------------------------------------------------------------------
-class Position;
-void refresh_accumulator(Position& pos);
+int nnue_evaluate_pos(Position* pos);
 
-// ---------------------------------------------------------------------------
-//  Incremental Accumulator Update
-// ---------------------------------------------------------------------------
-void update_accumulator_piece(Position& pos, Piece pc, Square sq, bool is_add);
+/************************************************************************
+*         EXTERNAL INTERFACES
+*
+* Load a NNUE file using
+*
+*   nnue_init(file_path)
+*
+* and then probe score using one of three functions, whichever
+* is convenient. From easy to hard
+*   
+*   a) nnue_evaluate_fen         - accepts a fen string for evaluation
+*   b) nnue_evaluate             - suitable for use in engines
+*   c) nnue_evaluate_incremental - for ultimate performance but will need
+*                                  some work on the engines side.
+*
+**************************************************************************/
+
+/**
+* Load NNUE file
+*/
+DLLExport void _CDECL nnue_init(
+  const char * evalFile             /** Path to NNUE file */
+);
+
+/**
+* Evaluate on FEN string
+* Returns
+*   Score relative to side to move in approximate centi-pawns
+*/
+DLLExport int _CDECL nnue_evaluate_fen(
+  const char* fen                   /** FEN string to probe evaluation for */
+);
+
+/**
+* Evaluation subroutine suitable for chess engines.
+* -------------------------------------------------
+* Piece codes are
+*     wking=1, wqueen=2, wrook=3, wbishop= 4, wknight= 5, wpawn= 6,
+*     bking=7, bqueen=8, brook=9, bbishop=10, bknight=11, bpawn=12,
+* Squares are
+*     A1=0, B1=1 ... H8=63
+* Input format:
+*     piece[0] is white king, square[0] is its location
+*     piece[1] is black king, square[1] is its location
+*     ..
+*     piece[x], square[x] can be in any order
+*     ..
+*     piece[n+1] is set to 0 to represent end of array
+* Returns
+*   Score relative to side to move in approximate centi-pawns
+*/
+DLLExport int _CDECL nnue_evaluate(
+  int player,                       /** Side to move: white=0 black=1 */
+  int* pieces,                      /** Array of pieces */
+  int* squares                      /** Corresponding array of squares each piece stands on */
+);
+
+/**
+* Incremental NNUE evaluation function.
+* -------------------------------------------------
+* First three parameters and return type are as in @nnue_evaluate
+*
+* nnue_data
+*    nnue_data[0] is pointer to NNUEdata for ply i.e. current position
+*    nnue_data[1] is pointer to NNUEdata for ply - 1
+*    nnue_data[2] is pointer to NNUEdata for ply - 2
+*/
+DLLExport int _CDECL nnue_evaluate_incremental(
+  int player,                       /** Side to move: white=0 black=1 */
+  int* pieces,                      /** Array of pieces */
+  int* squares,                     /** Corresponding array of squares each piece stands on */
+  NNUEdata** nnue_data              /** Pointer to NNUEdata* for current and previous plies */
+);
+
+#endif
